4d41d72aa95641983e5467d7e18229dd42a26d7e
Lucas Ilstedt <lucasilstedt@gmail.com>
2022-10-08 13:38:04 +0200

    Some progress on training.
    Also added crosshairs for vis indicator.
    Also removed old note now placed inline in TrainController.cs

--- Assets/Scripts/TrainController.cs
+++ Assets/Scripts/TrainController.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrainController : MonoBehaviour
 {
     [SerializeField] private float spawnTimer;
     [SerializeField] private int maxTargets;
-    [SerializeField] private GameObject targetPrefab, targetParent;
+    [SerializeField] private GameObject targetPrefab, targetParent, visIndicator;
+    [SerializeField] private Vector3[] spawnPoses;
+    [SerializeField] private Text feedback;
     private float timer;
+    private float beatTimer, beat = 1 / (109 / 60); // 1/(109/60) because EyeOfTheTiger is (supposedly) 109 BPM,
+                                                    // it feels kinda off but I'm unsure if it's performance, 
+                                                    // Time.deltaTime of wrong beat (did use an unofficial instrumental version).
     private int spawned;
     
     // Start is called before the first frame update
@@ -20,6 +26,7 @@ public class TrainController : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        beatTimer += Time.deltaTime;
 
         if(timer > spawnTimer)
         {
@@ -27,13 +34,51 @@ public class TrainController : MonoBehaviour
             if (spawned < maxTargets)
             {
                 //Spawn target
+                spawned++;
             }
         }
 
+        if(beatTimer >= beat)
+        {
+            visIndicator.SetActive(true);
+            beatTimer = 0;
+            StartCoroutine(Reset());
+        }
+    }
+
+    private IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        visIndicator.SetActive(false);
     }
 
     public void HitTarget()
     {
+        spawned--;
+        Destroy(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject);
+        if (visIndicator.activeSelf && beatTimer < 0.1)
+        {
+            feedback.text = "Perfect!";
+            StartCoroutine(ResetText());
+        }
+        else if (visIndicator.activeSelf && beatTimer <= 0.2)
+        {
+            feedback.text = "Good!";
+            StartCoroutine(ResetText());
+        }
+        else if (!visIndicator.activeSelf)
+        {
+            feedback.text = "Poor!";
+            StartCoroutine(ResetText());
+        }
 
+    }
+
+    private IEnumerator ResetText()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        feedback.text = string.Empty;
     }
 }
